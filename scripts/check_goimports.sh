713826e842c875b41f0d7ec34b86346b41b3255a
Andrew Wilkins <axwalk@gmail.com>
2018-07-30 10:53:22 +0800

    scripts: exclude vendor from goimports check

--- scripts/check_goimports.sh
+++ scripts/check_goimports.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-out=$(goimports -l -local github.com/elastic .)
+dirs=$(find . -maxdepth 1 -type d \! \( -name '.*' -or -name vendor \))
+out=$(goimports -l -local github.com/elastic *.go $dirs)
 if [ -n "$out" ]; then
   out=$(echo $out | sed 's/ /\n - /')
   printf "goimports differs:\n - $out\n" >&2
