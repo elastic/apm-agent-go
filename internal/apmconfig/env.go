521c15ca9fcb504241cc13f4be73b8f85d057fff
Andrew Wilkins <axwalk@gmail.com>
2018-09-14 16:18:44 +0800

    internal/apmconfig: add ParseSize/ParseSizeEnv
    Add a Size type, which represents a size in bytes,
    and a ParseSize and ParseSizeEnv functions.

--- internal/apmconfig/env.go
+++ internal/apmconfig/env.go
@@ -23,6 +23,21 @@ func ParseDurationEnv(envKey string, defaultDuration time.Duration) (time.Durati
 	return d, nil
 }
 
+// ParseSizeEnv gets the value of the environment variable envKey
+// and, if set, parses it as a size. If the environment variable
+// is unset, defaultSize is returned.
+func ParseSizeEnv(envKey string, defaultSize Size) (Size, error) {
+	value := os.Getenv(envKey)
+	if value == "" {
+		return defaultSize, nil
+	}
+	s, err := ParseSize(value)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to parse %s", envKey)
+	}
+	return s, nil
+}
+
 // ParseBoolEnv gets the value of the environment variable envKey
 // and, if set, parses it as a boolean. If the environment variable
 // is unset, defaultValue is returned.
