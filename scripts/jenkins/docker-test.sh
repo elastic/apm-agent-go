4023751075d131163e43c3ef1c7d711444b3dbab
Andrew Wilkins <axwalk@gmail.com>
2018-10-30 10:46:42 +0100

    Update scripts/jenkins/docker-test.sh
    Co-Authored-By: kuisathaverat <kuisathaverat@users.noreply.github.com>

--- scripts/jenkins/docker-test.sh
+++ scripts/jenkins/docker-test.sh
@@ -3,7 +3,6 @@ set -ex
 export GOPATH=$WORKSPACE
 eval "$(gvm $GO_VERSION)"
 go get -v -u github.com/jstemmer/go-junit-report
-go get -v -u github.com/axw/gocov/gocov
 go get -v -u github.com/matm/gocov-html
 go get -v -u github.com/axw/gocov/...
 go get -v -u github.com/AlekSi/gocov-xml
