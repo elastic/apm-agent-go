bbdaa726873a76f45c5fd6b4b864ee6e8b350885
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-11-08 14:57:06 +0100

    simplify script

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -10,8 +10,6 @@ jenkins_setup
 go get -v -u github.com/jstemmer/go-junit-report
 go get -v -t ./...
 
-export OUT_FILE="build/test-report.out"
 mkdir -p build
 
-(go test -race ./... -v 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
-cat ${OUT_FILE} | go-junit-report > build/junit-apm-agent-go.xml
+(go test -race ./... -v 2>&1 | go-junit-report > build/junit-apm-agent-go.xml) || echo -e "\033[31;49mTests FAILED\033[0m"
