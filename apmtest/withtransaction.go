e6a0136c217d2477439d8563b6d62ea00e3af35e
Andrew Wilkins <axwalk@gmail.com>
2019-07-29 11:26:42 +0800

    apmtest: add RecordingTracer.WithTransaction
    Add WithTransaction and WithTransactionOptions
    methods to RecordingTracer, and update the
    top-level functions to be implemented in terms
    of them. This enables calling those functions
    with a customised tracer.

--- apmtest/withtransaction.go
+++ apmtest/withtransaction.go
@@ -19,11 +19,9 @@ package apmtest
 
 import (
 	"context"
-	"fmt"
 
 	"go.elastic.co/apm"
 	"go.elastic.co/apm/model"
-	"go.elastic.co/apm/transport/transporttest"
 )
 
 // WithTransaction is equivalent to calling WithTransactionOptions with a zero TransactionOptions.
@@ -35,18 +33,7 @@ func WithTransaction(f func(ctx context.Context)) (model.Transaction, []model.Sp
 // and transaction options, flushes the transaction to a test server, and returns
 // the decoded transaction and any associated spans and errors.
 func WithTransactionOptions(opts apm.TransactionOptions, f func(ctx context.Context)) (model.Transaction, []model.Span, []model.Error) {
-	tracer, transport := transporttest.NewRecorderTracer()
+	tracer := NewRecordingTracer()
 	defer tracer.Close()
-
-	tx := tracer.StartTransactionOptions("name", "type", opts)
-	ctx := apm.ContextWithTransaction(context.Background(), tx)
-	f(ctx)
-
-	tx.End()
-	tracer.Flush(nil)
-	payloads := transport.Payloads()
-	if n := len(payloads.Transactions); n != 1 {
-		panic(fmt.Errorf("expected 1 transaction, got %d", n))
-	}
-	return payloads.Transactions[0], payloads.Spans, payloads.Errors
+	return tracer.WithTransactionOptions(opts, f)
 }
