4b3f3047ac8ca73fce1ac35424106c4a64e84b85
Victor Martinez <victormartinezrubio@gmail.com>
2020-01-09 13:02:30 +0000

    ci(jenkins): Install latest golang version for Windows (#702)

--- scripts/jenkins/windows/install-tools.bat
+++ scripts/jenkins/windows/install-tools.bat
@@ -3,4 +3,7 @@
 ::
 
 choco config set cacheLocation %WORKSPACE%
-choco install golang --version %GO_VERSION% -y --no-progress
+:: Install latest as long as https://github.com/chocolatey/chocolatey.org/issues/803
+:: is not resolved.
+:: choco install golang --version %GO_VERSION% -y --no-progress
+choco install golang -y --no-progress
--- scripts/jenkins/windows/uninstall-tools.bat
+++
@@ -1,5 +0,0 @@
-::
-:: This script install the required tools
-::
-
-choco uninstall golang --version %GO_VERSION% -y
