6133c5ef89a5b163177ec7efdb158f8a3f087183
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-06 12:51:51 +0000

    ci(jenkins): transform jUnit output for test stages

--- scripts/jenkins/build-test.sh
+++ scripts/jenkins/build-test.sh
@@ -5,4 +5,9 @@ set -euxo pipefail
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
-make install check
+export OUT_FILE="build/test-report.out"
+mkdir -p build
+make install check 2> >(tee ${OUT_FILE} 1>&2)
+
+go get -v -u github.com/jstemmer/go-junit-report
+go-junit-report > "build/junit-apm-agent-go-${GO_VERSION}.xml" < ${OUT_FILE}
