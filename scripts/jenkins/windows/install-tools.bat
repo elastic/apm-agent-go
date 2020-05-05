21f8bfaca4ff9330bb23ff83cd82cfbe3eb1656e
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-05 12:39:11 +0100

    fix: wrong path

--- scripts/jenkins/windows/install-tools.bat
+++ scripts/jenkins/windows/install-tools.bat
@@ -4,7 +4,7 @@
 
 if "%GO_VERSION%" == "1.14.2" (
     git clone https://github.com/v1v/chocolatey-packages .chocolatey-packages
-    cd chocolatey-packages\golang\1.14.2
+    cd .chocolatey-packages\golang\1.14.2
     choco pack
     choco install golang -s .
 ) else (
