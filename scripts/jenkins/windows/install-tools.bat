23a6e2768b7212405a883cd4bb1dd278b248f318
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-05 13:09:55 +0100

    choco: use the automation flags

--- scripts/jenkins/windows/install-tools.bat
+++ scripts/jenkins/windows/install-tools.bat
@@ -6,7 +6,7 @@ if "%GO_VERSION%" == "1.14.2" (
     git clone https://github.com/v1v/chocolatey-packages .chocolatey-packages
     cd .chocolatey-packages\golang\1.14.2
     choco pack
-    choco install golang -s .
+    choco install golang -y --no-progress -s .
 ) else (
     choco config set cacheLocation %WORKSPACE%
     choco install golang --version %GO_VERSION% -y --no-progress
