56cfae9455c4db58b58c6f20d1a138e2e2289f0b
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-05 08:28:47 +0100

    ci: install golang 1.14.2 in windows

--- scripts/jenkins/windows/install-tools.bat
+++ scripts/jenkins/windows/install-tools.bat
@@ -2,5 +2,12 @@
 :: This script install the required tools
 ::
 
-choco config set cacheLocation %WORKSPACE%
-choco install golang --version %GO_VERSION% -y --no-progress
+if "%GO_VERSION%" == "1.14.2" (
+    git clone https://github.com/v1v/chocolatey-packages .chocolatey-packages
+    cd chocolatey-packages\golang\1.14.2
+    choco pack
+    choco install golang -s .
+) else (
+    choco config set cacheLocation %WORKSPACE%
+    choco install golang --version %GO_VERSION% -y --no-progress
+)
