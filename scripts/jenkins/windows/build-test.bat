6ebed8bd952998ce01a2659d5337e9c411970edd
Andrew Wilkins <axw@elastic.co>
2020-09-13 09:26:07 +0800

    scripts/jenkins/windows: fix redirection syntax

--- scripts/jenkins/windows/build-test.bat
+++ scripts/jenkins/windows/build-test.bat
@@ -5,7 +5,7 @@
 go get -v -u github.com/jstemmer/go-junit-report
 go get -t ./...
 mkdir build
-go test -v ./... &> build\\test.out
+go test -v ./... > build\\test.out 2>&1
 type build\\test.out
 type build\\test.out | go-junit-report > build\\junit-apm-agent-go.xml
 type build\\junit-apm-agent-go.xml
