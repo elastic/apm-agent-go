46d8c0a6b434271456132df2def87b6d5a08584d
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2020-12-07 20:00:03 +0800

    Added support for go-pg v10 (#857)
    * Added support for v10

---
+++ module/apmgopgv10/go.mod
@@ -0,0 +1,14 @@
+module go.elastic.co/apm/module/apmgopgv10
+
+require (
+	github.com/go-pg/pg/v10 v10.7.3
+	github.com/stretchr/testify v1.6.1
+	go.elastic.co/apm v1.9.0
+	go.elastic.co/apm/module/apmsql v1.9.0
+)
+
+replace go.elastic.co/apm => ../..
+
+replace go.elastic.co/apm/module/apmsql => ../apmsql
+
+go 1.14
---
+++ module/apmgopgv10/hook.go
@@ -0,0 +1,89 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// +build go1.14
+
+package apmgopgv10 // import "go.elastic.co/apm/module/apmgopgv10"
+
+import (
+	"context"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/pkg/errors"
+
+	"go.elastic.co/apm"
+	"go.elastic.co/apm/module/apmsql"
+	"go.elastic.co/apm/stacktrace"
+)
+
+func init() {
+	stacktrace.RegisterLibraryPackage("github.com/go-pg/pg/v10")
+}
+
+// Instrument modifies db such that operations are hooked and reported as spans
+// to Elastic APM if they occur within the context of a captured transaction.
+//
+// If Instrument cannot instrument db, then an error will be returned.
+func Instrument(db *pg.DB) error {
+	db.AddQueryHook(&queryHook{})
+
+	return nil
+}
+
+var _ pg.QueryHook = (*queryHook)(nil)
+
+// queryHook is an implementation of pg.QueryHook that reports queries as spans to Elastic APM.
+type queryHook struct{}
+
+// BeforeQuery initiates the span for the database query
+func (qh *queryHook) BeforeQuery(ctx context.Context, evt *pg.QueryEvent) (context.Context, error) {
+	var (
+		database string
+		user     string
+	)
+	if db, ok := evt.DB.(*pg.DB); ok {
+		opts := db.Options()
+		user = opts.User
+		database = opts.Database
+	}
+
+	sql, err := evt.UnformattedQuery()
+	if err != nil {
+		return ctx, errors.Wrap(err, "failed to generate sql")
+	}
+
+	span, ctx := apm.StartSpan(ctx, apmsql.QuerySignature(string(sql)), "db.postgresql.query")
+	span.Context.SetDatabase(apm.DatabaseSpanContext{
+		Statement: string(sql),
+
+		// Static
+		Type:     "sql",
+		User:     user,
+		Instance: database,
+	})
+
+	return ctx, nil
+}
+
+// AfterQuery ends the initiated span from BeforeQuery
+func (qh *queryHook) AfterQuery(ctx context.Context, evt *pg.QueryEvent) error {
+	if span := apm.SpanFromContext(ctx); span != nil {
+		span.End()
+	}
+
+	return nil
+}
