e56b0137782f3042cff97b0d4aededd25853afcf
Andrew Wilkins <axw@elastic.co>
2021-02-17 10:06:07 +0800

    internal/apmgodog: implement outcome spec

--- internal/apmgodog/go.mod
+++ internal/apmgodog/go.mod
@@ -5,6 +5,9 @@ go 1.13
 require (
 	github.com/cucumber/godog v0.8.1
 	go.elastic.co/apm v1.11.0
+	go.elastic.co/apm/module/apmgrpc v1.11.0
+	go.elastic.co/apm/module/apmhttp v1.11.0
+	google.golang.org/grpc v1.17.0
 )
 
 replace go.elastic.co/apm => ../..
--- internal/apmgodog/suitecontext.go
+++ internal/apmgodog/suitecontext.go
@@ -15,41 +15,130 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// +build go1.9
+
 package apmgodog
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/cucumber/godog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/test/bufconn"
 
 	"go.elastic.co/apm"
+	"go.elastic.co/apm/apmtest"
+	"go.elastic.co/apm/module/apmgrpc"
+	"go.elastic.co/apm/module/apmhttp"
 	"go.elastic.co/apm/transport"
 )
 
 type featureContext struct {
 	apiKey      string
 	secretToken string
+
+	httpServer  *httptest.Server
+	httpHandler *httpHandler
+
+	grpcServer  *grpc.Server
+	grpcClient  *grpc.ClientConn
+	grpcService *helloworldGRPCService
+
+	tracer      *apmtest.RecordingTracer
+	span        *apm.Span
+	transaction *apm.Transaction
 }
 
 // InitContext initialises a godoc.Suite with step definitions.
 func InitContext(s *godog.Suite) {
-	c := &featureContext{}
+	c := &featureContext{
+		tracer: apmtest.NewRecordingTracer(),
+	}
+
+	c.httpHandler = &httpHandler{}
+	c.httpServer = httptest.NewServer(
+		apmhttp.Wrap(c.httpHandler, apmhttp.WithTracer(c.tracer.Tracer)),
+	)
+
+	c.grpcServer = grpc.NewServer(grpc.UnaryInterceptor(
+		apmgrpc.NewUnaryServerInterceptor(
+			apmgrpc.WithTracer(c.tracer.Tracer),
+			apmgrpc.WithRecovery(),
+		),
+	))
+	c.grpcService = &helloworldGRPCService{}
+	pb.RegisterGreeterServer(c.grpcServer, c.grpcService)
+	grpcListener := bufconn.Listen(1)
+	grpcClient, err := grpc.Dial("bufconn",
+		grpc.WithInsecure(),
+		grpc.WithDialer(func(string, time.Duration) (net.Conn, error) { return grpcListener.Dial() }),
+		grpc.WithUnaryInterceptor(apmgrpc.NewUnaryClientInterceptor()),
+	)
+	if err != nil {
+		panic(err)
+	}
+	c.grpcClient = grpcClient
+	go c.grpcServer.Serve(grpcListener)
+
 	s.BeforeScenario(func(interface{}) { c.reset() })
+	s.AfterSuite(func() {
+		c.tracer.Close()
+		c.grpcServer.Stop()
+		c.grpcClient.Close()
+		c.httpServer.Close()
+	})
 
 	s.Step("^an agent$", c.anAgent)
 	s.Step("^an api key is not set in the config$", func() error { return nil })
 	s.Step("^an api key is set to '(.*)' in the config$", c.setAPIKey)
 	s.Step("^a secret_token is set to '(.*)' in the config$", c.setSecretToken)
 	s.Step("^the Authorization header is '(.*)'$", c.checkAuthorizationHeader)
+
+	s.Step("^an active span$", c.anActiveSpan)
+	s.Step("^an active transaction$", c.anActiveTransaction)
+
+	s.Step("^user sets span outcome to '(.*)'$", c.userSetsSpanOutcome)
+	s.Step("^user sets transaction outcome to '(.*)'$", c.userSetsTransactionOutcome)
+	s.Step("^span terminates with outcome '(.*)'$", c.spanTerminatesWithOutcome)
+	s.Step("^transaction terminates with outcome '(.*)'$", c.transactionTerminatesWithOutcome)
+	s.Step("^span terminates with an error$", func() error { return c.spanTerminatesWithOutcome("failure") })
+	s.Step("^span terminates without error$", func() error { return c.spanTerminatesWithOutcome("success") })
+	s.Step("^transaction terminates with an error$", func() error { return c.transactionTerminatesWithOutcome("failure") })
+	s.Step("^transaction terminates without error$", func() error { return c.transactionTerminatesWithOutcome("success") })
+	s.Step("^span outcome is '(.*)'$", c.spanOutcomeIs)
+	s.Step("^span outcome is \"(.*)\"$", c.spanOutcomeIs)
+	s.Step("^transaction outcome is '(.*)'$", c.transactionOutcomeIs)
+	s.Step("^transaction outcome is \"(.*)\"$", c.transactionOutcomeIs)
+
+	s.Step("^an HTTP transaction with (.*) response code$", c.anHTTPTransactionWithStatusCode)
+	s.Step("^an HTTP span with (.*) response code$", c.anHTTPSpanWithStatusCode)
+
+	s.Step("^a gRPC transaction with '(.*)' status$", c.aGRPCTransactionWithStatusCode)
+	s.Step("^a gRPC span with '(.*)' status$", c.aGRPCSpanWithStatusCode)
 }
 
 func (c *featureContext) reset() {
 	c.apiKey = ""
 	c.secretToken = ""
+	c.span = nil
+	c.transaction = nil
+	c.httpHandler.panic = false
+	c.httpHandler.statusCode = http.StatusOK
+	c.grpcService.panic = false
+	c.grpcService.err = nil
+	c.tracer.ResetPayloads()
+
 	for _, k := range os.Environ() {
 		if strings.HasPrefix(k, "ELASTIC_APM") {
 			os.Unsetenv(k)
@@ -86,7 +175,6 @@ func (c *featureContext) checkAuthorizationHeader(expected string) error {
 	if _, err := transport.InitDefault(); err != nil {
 		return err
 	}
-
 	tracer, err := apm.NewTracer("godog", "")
 	if err != nil {
 		return err
@@ -103,4 +191,168 @@ func (c *featureContext) checkAuthorizationHeader(expected string) error {
 		return fmt.Errorf("got Authorization header value %q, expected %q", authHeader, expected)
 	}
 	return nil
+}
+
+func (c *featureContext) anActiveSpan() error {
+	if err := c.anActiveTransaction(); err != nil {
+		return err
+	}
+	c.span = c.transaction.StartSpan("name", "type", nil)
+	return nil
+}
+
+func (c *featureContext) anActiveTransaction() error {
+	c.transaction = c.tracer.StartTransaction("name", "type")
+	return nil
+}
+
+func (c *featureContext) userSetsSpanOutcome(outcome string) error {
+	c.span.Outcome = outcome
+	return nil
+}
+
+func (c *featureContext) userSetsTransactionOutcome(outcome string) error {
+	c.transaction.Outcome = outcome
+	return nil
+}
+
+func (c *featureContext) spanTerminatesWithOutcome(outcome string) error {
+	switch outcome {
+	case "unknown":
+	case "success":
+		c.span.Context.SetHTTPStatusCode(200)
+	case "failure":
+		c.span.Context.SetHTTPStatusCode(400)
+	}
+	c.span.End()
+	return nil
+}
+
+func (c *featureContext) transactionTerminatesWithOutcome(outcome string) error {
+	switch outcome {
+	case "unknown":
+	case "success":
+		c.transaction.Context.SetHTTPStatusCode(200)
+	case "failure":
+		c.transaction.Context.SetHTTPStatusCode(500)
+	}
+	c.transaction.End()
+	return nil
+}
+
+func (c *featureContext) spanOutcomeIs(expected string) error {
+	c.tracer.Flush(nil)
+	payloads := c.tracer.Payloads()
+	if actual := payloads.Spans[0].Outcome; actual != expected {
+		return fmt.Errorf("expected outcome %q, got %q", expected, actual)
+	}
+	return nil
+}
+
+func (c *featureContext) transactionOutcomeIs(expected string) error {
+	c.tracer.Flush(nil)
+	payloads := c.tracer.Payloads()
+	if actual := payloads.Transactions[0].Outcome; actual != expected {
+		return fmt.Errorf("expected outcome %q, got %q", expected, actual)
+	}
+	return nil
+}
+
+func (c *featureContext) anHTTPTransactionWithStatusCode(statusCode int) error {
+	if statusCode < 0 {
+		c.httpHandler.panic = true
+	} else {
+		c.httpHandler.statusCode = statusCode
+	}
+	return c.sendHTTPRequest(context.Background())
+}
+
+func (c *featureContext) anHTTPSpanWithStatusCode(statusCode int) error {
+	if statusCode < 0 {
+		c.httpHandler.panic = true
+	}
+	tx := c.tracer.StartTransaction("name", "type")
+	defer tx.End()
+	ctx := apm.ContextWithTransaction(context.Background(), tx)
+	return c.sendHTTPRequest(ctx)
+}
+
+func (c *featureContext) sendHTTPRequest(ctx context.Context) error {
+	client := apmhttp.WrapClient(c.httpServer.Client())
+	req, _ := http.NewRequest("GET", c.httpServer.URL, nil)
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+	return resp.Body.Close()
+}
+
+func (c *featureContext) aGRPCTransactionWithStatusCode(statusCode string) error {
+	if statusCode == "n/a" {
+		c.grpcService.panic = true
+	} else {
+		code, err := parseGRPCStatusCode(statusCode)
+		if err != nil {
+			return err
+		}
+		if code != codes.OK {
+			c.grpcService.err = status.Error(code, code.String())
+		}
+	}
+	pb.NewGreeterClient(c.grpcClient).SayHello(context.Background(), &pb.HelloRequest{Name: "world"})
+	return nil
+}
+
+func (c *featureContext) aGRPCSpanWithStatusCode(statusCode string) error {
+	if statusCode == "n/a" {
+		c.grpcService.panic = true
+	} else {
+		code, err := parseGRPCStatusCode(statusCode)
+		if err != nil {
+			return err
+		}
+		if code != codes.OK {
+			c.grpcService.err = status.Error(code, code.String())
+		}
+	}
+	tx := c.tracer.StartTransaction("name", "type")
+	defer tx.End()
+	ctx := apm.ContextWithTransaction(context.Background(), tx)
+	pb.NewGreeterClient(c.grpcClient).SayHello(ctx, &pb.HelloRequest{Name: "world"})
+	return nil
+}
+
+func parseGRPCStatusCode(s string) (codes.Code, error) {
+	var code codes.Code
+	err := code.UnmarshalJSON([]byte(strconv.Quote(s)))
+	return code, err
+}
+
+type helloworldGRPCService struct {
+	panic bool
+	err   error
+}
+
+func (h *helloworldGRPCService) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	if h.panic {
+		panic("boom")
+	}
+	return &pb.HelloReply{Message: "hello, " + req.Name}, h.err
+}
+
+type httpHandler struct {
+	panic      bool
+	statusCode int
+}
+
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.panic {
+		panic("boom")
+	}
+	statusCode := h.statusCode
+	if statusCode == 100 {
+		// Cheat to avoid complexity of dealing with 100 Continue responses.
+		statusCode = 101
+	}
+	w.WriteHeader(statusCode)
 }
