elastic/apm-agent-go

commit 1d2828e5b68557ca4bf316b79fefb35c84f20e65

Capture request body in CaptureError calls (#906)

capturebody.go
@@ -136,15 +136,15 @@ func (bc *BodyCapturer) Discard() {
 	bodyCapturerPool.Put(bc)
 }
 
-func (bc *BodyCapturer) setContext(out *model.RequestBody) bool {
-	if bc.request.PostForm != nil {
+func (bc *BodyCapturer) setContext(out *model.RequestBody, req *http.Request) bool {
+	if req.PostForm != nil {
 		// We must copy the map in case we need to
 		// sanitize the values. Ideally we should only
 		// copy if sanitization is necessary, but body
 		// capture shouldn't typically be enabled so
 		// we don't currently optimize this.
-		postForm := make(url.Values, len(bc.request.PostForm))
-		for k, v := range bc.request.PostForm {
+		postForm := make(url.Values, len(req.PostForm))
+		for k, v := range req.PostForm {
 			vcopy := make([]string, len(v))
 			for i := range vcopy {
 				vcopy[i] = truncateString(v[i])

apmtest/httpsuite.go
@@ -20,11 +20,13 @@ package apmtest // import "go.elastic.co/apm/apmtest"
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"go.elastic.co/apm"
+	"go.elastic.co/apm/model"
 	"go.elastic.co/apm/transport/transporttest"
 )
 
@@ -38,6 +40,7 @@ type HTTPTestSuite struct {
 	//   GET /implicit_write (no explicit write on the response)
 	//   GET /panic_before_write (panic without writing response)
 	//   GET /panic_after_write (panic after writing response)
+	//	 POST /explicit_error_capture (explicit CaptureError call)
 	//
 	Handler http.Handler
 
@@ -134,4 +137,27 @@ func (s *HTTPTestSuite) TestPanicAfterWrite() {
 	e := ps.Errors[0]
 	s.Equal(tx.ID, e.ParentID)
 	s.Equal(resp.StatusCode, e.Context.Response.StatusCode)
+}
+
+// TestExplicitErrorCapture tests that a CaptureError explicit call
+// inside an HTTP request transaction captures the request body
+func (s *HTTPTestSuite) TestExplicitErrorCapture() {
+	s.Tracer.SetCaptureBody(apm.CaptureBodyAll)
+	resp, err := http.Post(
+		s.server.URL+"/explicit_error_capture",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("body"),
+	)
+	require.NoError(s.T(), err)
+	defer resp.Body.Close()
+	s.Equal(http.StatusServiceUnavailable, resp.StatusCode)
+
+	s.Tracer.Flush(nil)
+	ps := s.Recorder.Payloads()
+	require.Len(s.T(), ps.Transactions, 1)
+
+	tx := ps.Transactions[0]
+	e := ps.Errors[0]
+	s.Equal(&model.RequestBody{Raw: "body"}, tx.Context.Request.Body)
+	s.Equal(&model.RequestBody{Raw: "body"}, e.Context.Request.Body)
 }

context.go
@@ -32,6 +32,7 @@ import (
 type Context struct {
 	model               model.Context
 	request             model.Request
+	httpRequest         *http.Request
 	requestBody         model.RequestBody
 	requestSocket       model.RequestSocket
 	response            model.Response
@@ -171,6 +172,8 @@ func (c *Context) SetHTTPRequest(req *http.Request) {
 		httpVersion = fmt.Sprintf("%d.%d", req.ProtoMajor, req.ProtoMinor)
 	}
 
+	c.httpRequest = req
+
 	c.request = model.Request{
 		Body:        c.request.Body,
 		URL:         apmhttputil.RequestURL(req),
@@ -216,7 +219,7 @@ func (c *Context) SetHTTPRequestBody(bc *BodyCapturer) {
 	if bc == nil || bc.captureBody&c.captureBodyMask == 0 {
 		return
 	}
-	if bc.setContext(&c.requestBody) {
+	if bc.setContext(&c.requestBody, c.httpRequest) {
 		c.request.Body = &c.requestBody
 	}
 }

gocontext.go
@@ -23,6 +23,8 @@ import (
 	"go.elastic.co/apm/internal/apmcontext"
 )
 
+type bodyCapturerKey struct{}
+
 // ContextWithSpan returns a copy of parent in which the given span
 // is stored, associated with the key ContextSpanKey.
 func ContextWithSpan(parent context.Context, s *Span) context.Context {
@@ -33,6 +35,12 @@ func ContextWithSpan(parent context.Context, s *Span) context.Context {
 // transaction is stored, associated with the key ContextTransactionKey.
 func ContextWithTransaction(parent context.Context, t *Transaction) context.Context {
 	return apmcontext.ContextWithTransaction(parent, t)
+}
+
+// ContextWithBodyCapturer returns a copy of parent in which the given
+// body capturer is stored, associated with the key bodyCapturerKey.
+func ContextWithBodyCapturer(parent context.Context, bc *BodyCapturer) context.Context {
+	return context.WithValue(parent, bodyCapturerKey{}, bc)
 }
 
 // SpanFromContext returns the current Span in context, if any. The span must
@@ -48,6 +56,11 @@ func SpanFromContext(ctx context.Context) *Span {
 // ContextWithTransaction.
 func TransactionFromContext(ctx context.Context) *Transaction {
 	value, _ := apmcontext.TransactionFromContext(ctx).(*Transaction)
+	return value
+}
+
+func bodyCapturerFromContext(ctx context.Context) *BodyCapturer {
+	value, _ := ctx.Value(bodyCapturerKey{}).(*BodyCapturer)
 	return value
 }
 
@@ -130,6 +143,11 @@ func CaptureError(ctx context.Context, err error) *Error {
 		}
 		e := tx.tracer.NewError(err)
 		e.Handled = true
+		bc := bodyCapturerFromContext(ctx)
+		if bc != nil {
+			e.Context.SetHTTPRequest(bc.request)
+			e.Context.SetHTTPRequestBody(bc)
+		}
 		e.SetTransaction(tx)
 		return e
 	} else {

internal/apmcontext/context.go
@@ -17,7 +17,9 @@
 
 package apmcontext
 
-import "context"
+import (
+	"context"
+)
 
 var (
 	// ContextWithSpan takes a context and span and returns a new context

module/apmecho/middleware.go
@@ -70,10 +70,9 @@ func (m *middleware) handle(c echo.Context) error {
 		return m.handler(c)
 	}
 	name := req.Method + " " + c.Path()
-	tx, req := apmhttp.StartTransaction(m.tracer, name, req)
+	tx, body, req := apmhttp.StartTransactionWithBody(m.tracer, name, req)
 	defer tx.End()
 	c.SetRequest(req)
-	body := m.tracer.CaptureHTTPRequestBody(req)
 
 	resp := c.Response()
 	var handlerErr error

module/apmecho/middleware_test.go
@@ -19,9 +19,12 @@ package apmecho_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"go.elastic.co/apm"
 
 	"github.com/labstack/echo"
 	"github.com/pkg/errors"
@@ -44,6 +47,12 @@ func TestMiddlewareHTTPSuite(t *testing.T) {
 	e.GET("/panic_after_write", func(c echo.Context) error {
 		c.String(200, "hello, world")
 		panic("boom")
+	})
+	e.POST("/explicit_error_capture", func(c echo.Context) error {
+		ioutil.ReadAll(c.Request().Body)
+		e := apm.CaptureError(c.Request().Context(), errors.New("total explosion"))
+		e.Send()
+		return c.String(503, e.Error())
 	})
 	suite.Run(t, &apmtest.HTTPTestSuite{
 		Handler:  e,

module/apmechov4/middleware.go
@@ -70,10 +70,9 @@ func (m *middleware) handle(c echo.Context) error {
 		return m.handler(c)
 	}
 	name := req.Method + " " + c.Path()
-	tx, req := apmhttp.StartTransaction(m.tracer, name, req)
+	tx, body, req := apmhttp.StartTransactionWithBody(m.tracer, name, req)
 	defer tx.End()
 	c.SetRequest(req)
-	body := m.tracer.CaptureHTTPRequestBody(req)
 
 	resp := c.Response()
 	var handlerErr error

commit 2e0cbebb1c9b7ef00fd32f0aa5ee2f41de6e5e9f

BodyCapturer: Fix NPE on benchmark when nil Context.request
Fixes a nil pointer exception on the benchmark `BenchmarkBodyCapturer`.
The `Context.request` field wasn't set in the benchmark and is being
passed in the `BodyCapturer.setContext()` call as nil, causing an NPE
when `req.PostForm` is accessed.

The bug seems to have been introduced in #906.

This might not be the right fix since I'm not very familiar with the
codebase.

Signed-off-by: Marc Lopez <marc5.12@outlook.com>

capturebody.go
@@ -137,7 +137,7 @@ func (bc *BodyCapturer) Discard() {
 }
 
 func (bc *BodyCapturer) setContext(out *model.RequestBody, req *http.Request) bool {
-	if req.PostForm != nil {
+	if req != nil && req.PostForm != nil {
 		// We must copy the map in case we need to
 		// sanitize the values. Ideally we should only
 		// copy if sanitization is necessary, but body