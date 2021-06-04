df2c95d704888dc2ab53f7c494a9da36be36e47b
Sergio Andrés Virviescas Santana <developersavsgio@gmail.com>
2021-06-04 11:05:00 +0200

    Update module/apmfasthttp/utils.go
    Co-authored-by: Andrew Wilkins <axwalk@gmail.com>

--- module/apmfasthttp/utils.go
+++ module/apmfasthttp/utils.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package apmfasthttp
+package apmfasthttp // import "go.elastic.co/apm/module/apmfasthttp"
 
 import (
 	"net/url"
