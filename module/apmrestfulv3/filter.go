239b62e8ee5f6708713e9e26c4111f77b5034df2
Luqman Setyo N <38354405+luqmansen@users.noreply.github.com>
2021-06-08 09:56:13 +0700

    fix: change build tags for apmrestfulv3 to go1.11 (#971)

--- module/apmrestfulv3/filter.go
+++ module/apmrestfulv3/filter.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// +build go1.14
+// +build go1.11
 
 package apmrestfulv3 // import "go.elastic.co/apm/module/apmrestfulv3"
 
