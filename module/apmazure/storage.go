5a92a9c8abcd8795ab80e55d578e6da024638481
stuart nelson <stuartnelson3@gmail.com>
2021-08-17 10:20:55 +0200

    Add build constraints
    Some of the azure go sdk modules require go1.14

--- module/apmazure/storage.go
+++ module/apmazure/storage.go
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// +build go1.14
+
 package apmazure // import "go.elastic.co/apm/module/apmazure"
 
 import (
--- module/apmazure/storage_test.go
+++ module/apmazure/storage_test.go
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// +build go1.14
+
 package apmazure
 
 import (
