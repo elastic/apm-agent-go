47c5f6ce72cba00192acfb4ca5892c1e049d0d52
Marc Lopez Rubio <marc5.12@outlook.com>
2021-10-05 13:35:01 +0800

    Add tests and test debug helpers
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

---
+++ apmtest/debug.go
@@ -0,0 +1,158 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package apmtest // import "go.elastic.co/apm/apmtest"
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+	"sort"
+	"text/tabwriter"
+	"time"
+	"unicode/utf8"
+
+	"go.elastic.co/apm/model"
+)
+
+// PrintTraces displays the trace as a table which can be used on tests to aid
+// debugging.
+func PrintTraces(writer io.Writer, tx model.Transaction, spans []model.Span) {
+	w := tabwriter.NewWriter(writer, 2, 4, 2, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w, "#\tNAME\tTYPE\tCOMP\tN\tDURATION\tOFFSET\tSPAN ID\tPARENT ID\tTRACE ID")
+
+	fmt.Fprintf(w, "TX\t%s\t%s\t-\t-\t%f\t%d\t%x\t%x\t%x\n", tx.Name,
+		tx.Type, tx.Duration,
+		0,
+		tx.ID, tx.ParentID, tx.TraceID,
+	)
+
+	sort.SliceStable(spans, func(i, j int) bool {
+		return time.Time(spans[i].Timestamp).Before(time.Time(spans[j].Timestamp))
+	})
+	for i, span := range spans {
+		count := 1
+		if span.Composite != nil {
+			count = span.Composite.Count
+		}
+
+		fmt.Fprintf(w, "%d\t%s\t%s\t%v\t%d\t%f\t+%d\t%x\t%x\t%x\n", i, span.Name,
+			span.Type, span.Composite != nil, count, span.Duration,
+			time.Time(span.Timestamp).Sub(time.Time(tx.Timestamp))/1e3,
+			span.ID, span.ParentID, span.TraceID,
+		)
+	}
+	w.Flush()
+}
+
+// WriteTraceWaterfall the trace waterfall "console output" to the specified
+// writer sorted by timestamp.
+func WriteTraceWaterfall(w io.Writer, tx model.Transaction, spans []model.Span) {
+	maxDuration := time.Duration(tx.Duration * float64(time.Millisecond))
+	if maxDuration == 0 {
+		for _, span := range spans {
+			maxDuration += time.Duration(span.Duration * float64(time.Millisecond))
+		}
+	}
+
+	maxWidth := int64(220)
+	buf := new(bytes.Buffer)
+	if tx.Duration > 0.0 {
+		writeSpan(buf, int(maxWidth), 0, fmt.Sprintf("transaction (%x) - %s", tx.ID, maxDuration.String()))
+	}
+
+	sort.SliceStable(spans, func(i, j int) bool {
+		return time.Time(spans[i].Timestamp).Before(time.Time(spans[j].Timestamp))
+	})
+
+	for _, span := range spans {
+		pos := int(math.Round(
+			float64(time.Time(span.Timestamp).Sub(time.Time(tx.Timestamp))) /
+				float64(maxDuration) * float64(maxWidth),
+		))
+		tDur := time.Duration(span.Duration * float64(time.Millisecond))
+		dur := float64(tDur) / float64(maxDuration)
+		width := int(math.Round(dur * float64(maxWidth)))
+		if width == int(maxWidth) {
+			width = int(maxWidth) - 1
+		}
+
+		spancontent := fmt.Sprintf("%s %s - %s",
+			span.Type, span.Name,
+			time.Duration(span.Duration*float64(time.Millisecond)).String(),
+		)
+		if span.Composite != nil {
+			spancontent = fmt.Sprintf("%d*%s", span.Composite.Count, spancontent)
+		}
+		writeSpan(buf, width, pos, spancontent)
+	}
+
+	io.Copy(w, buf)
+}
+
+func writeSpan(buf *bytes.Buffer, width, pos int, content string) {
+	spaceRune := ' '
+	fillRune := ' '
+	startRune := '['
+	endRune := ']'
+
+	// Prevent the spans from going out of bounds.
+	if pos == width {
+		pos = pos - 2
+	} else if pos >= width {
+		pos = pos - 1
+	}
+
+	for i := 0; i < int(pos); i++ {
+		buf.WriteRune(spaceRune)
+	}
+
+	if width <= 1 {
+		width = 1
+		// Write the first letter of the span type when the width is too small.
+		startRune, _ = utf8.DecodeRuneInString(content)
+	}
+
+	var written int
+	written, _ = buf.WriteRune(startRune)
+	if len(content) >= int(width)-1 {
+		content = content[:int(width)-1]
+	}
+
+	spacing := (width - len(content) - 2) / 2
+	for i := 0; i < spacing; i++ {
+		n, _ := buf.WriteRune(fillRune)
+		written += n
+	}
+
+	n, _ := buf.WriteString(content)
+	written += n
+	for i := 0; i < spacing; i++ {
+		n, _ := buf.WriteRune(fillRune)
+		written += n
+	}
+
+	if written < width {
+		buf.WriteRune(fillRune)
+	}
+	if width > 1 {
+		buf.WriteRune(endRune)
+	}
+
+	buf.WriteString("\n")
+}
--- span_test.go
+++ span_test.go
@@ -19,6 +19,8 @@ package apm_test
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -188,6 +190,259 @@ func TestStartExitSpan(t *testing.T) {
 	// Spans should still be marked as an exit span after they've been
 	// ended.
 	assert.True(t, span.IsExitSpan())
+}
+
+func TestCompressSpanExactMatch(t *testing.T) {
+	tests := []struct {
+		name               string
+		compressionEnabled bool
+		setup              func(t *testing.T) func()
+		assertFunc         func(t *testing.T, tx model.Transaction, spans []model.Span)
+	}{
+		{
+			name:               "CompressFalse",
+			compressionEnabled: false,
+			assertFunc: func(t *testing.T, tx model.Transaction, spans []model.Span) {
+				require.NotEmpty(t, tx)
+				require.Equal(t, 14, len(spans))
+				for _, span := range spans {
+					require.Nil(t, span.Composite)
+				}
+			},
+		},
+		{
+			name:               "CompressTrueSettingTweak",
+			compressionEnabled: true,
+			setup: func(t *testing.T) func() {
+				envVarName := "ELASTIC_APM_SPAN_COMPRESSION_EXACT_MATCH_MAX_DURATION"
+				og := os.Getenv(envVarName)
+				os.Setenv(envVarName, "1ms")
+				return func() { os.Setenv(envVarName, og) }
+			},
+			assertFunc: func(t *testing.T, tx model.Transaction, spans []model.Span) {
+				require.NotEmpty(t, tx)
+				// This setting
+				require.Equal(t, 5, len(spans))
+				for _, span := range spans[1:] {
+					require.Nil(t, span.Composite)
+				}
+			},
+		},
+		{
+			name:               "CompressSpanCount4",
+			compressionEnabled: true,
+			assertFunc: func(t *testing.T, tx model.Transaction, spans []model.Span) {
+				var composite = spans[0]
+				assert.Equal(t, composite.Context.Destination.Service.Resource, "mysql")
+				compositeSpanCount := 11
+				assert.Equal(t, composite.Composite.Count, compositeSpanCount)
+				assert.Equal(t, composite.Composite.CompressionStrategy, "exact_match")
+				// Sum should be at least the time that each span ran for.
+				assert.Greater(t, composite.Composite.Sum,
+					float64(int64(compositeSpanCount)*100*time.Nanosecond.Milliseconds()),
+				)
+
+				for _, span := range spans {
+					if span.Type == "mysql" {
+						continue
+					}
+					assert.Nil(t, span.Composite)
+				}
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.setup != nil {
+				t.Cleanup(test.setup(t))
+			}
+
+			tracer := apmtest.NewRecordingTracer()
+			tracer.SetSpanCompressionEnabled(test.compressionEnabled)
+
+			// When compression is enabled:
+			// Compress 10 spans into 1 and add another span with a different type
+			// [                    Transaction                    ]
+			//  [ mysql (11) ] [ request ] [ internal ] [ request ]
+			//
+			tx, spans, _ := tracer.WithTransaction(func(ctx context.Context) {
+				exitSpanOpt := apm.SpanOptions{ExitSpan: true}
+				for i := 0; i < 10; i++ {
+					span, _ := apm.StartSpanOptions(ctx, "SELECT * FROM users", "mysql", exitSpanOpt)
+					<-time.After(100 * time.Nanosecond)
+					span.End()
+				}
+				{
+					span, _ := apm.StartSpanOptions(ctx, "SELECT * FROM users", "mysql", exitSpanOpt)
+					<-time.After(2 * time.Millisecond)
+					span.End()
+				}
+
+				// None of these should be added to the composite.
+				{
+					span, _ := apm.StartSpanOptions(ctx, "GET /", "request", exitSpanOpt)
+					<-time.After(100 * time.Nanosecond)
+					span.End()
+				}
+				{
+					// Not an exit span, should not be compressed
+					span, _ := apm.StartSpan(ctx, "calculate complex", "internal")
+					<-time.After(100 * time.Nanosecond)
+					span.End()
+				}
+				{
+					// Exit span, this is a good candidate to be compressed, but
+					// since it can't be compressed with the last request type ("internal")
+					span, _ := apm.StartSpanOptions(ctx, "GET /", "request", exitSpanOpt)
+					<-time.After(100 * time.Nanosecond)
+					span.End()
+				}
+			})
+			defer func() {
+				if t.Failed() {
+					apmtest.WriteTraceWaterfall(os.Stdout, tx, spans)
+				}
+			}()
+
+			if test.assertFunc != nil {
+				test.assertFunc(t, tx, spans)
+			}
+		})
+	}
+}
+
+func TestCompressSpanSameKind(t *testing.T) {
+	tracer := apmtest.NewRecordingTracer()
+	tracer.SetSpanCompressionEnabled(true)
+
+	// Compress 5 spans into 1 and add another span with a different type
+	// [       Transaction       ]
+	//  [ mysql ] [ request (5) ]
+	//
+	tx, spans, _ := tracer.WithTransaction(func(ctx context.Context) {
+		exitSpanOpt := apm.SpanOptions{ExitSpan: true}
+		path := []string{"/a", "/b", "/c", "/d", "/e"}
+		// Span is compressable, but cannot be compressed since the next span
+		// is not the same kind. It gets published.
+		{
+			span, _ := apm.StartSpanOptions(ctx, "SELECT * FROM users", "mysql", exitSpanOpt)
+			<-time.After(100 * time.Nanosecond)
+			span.End()
+		}
+		// These spans should be compressed into 1.
+		for i := 0; i < 5; i++ {
+			uri := fmt.Sprint("GET ", path[i])
+			span, _ := apm.StartSpanOptions(ctx, uri, "request", exitSpanOpt)
+			<-time.After(100 * time.Nanosecond)
+			span.End()
+		}
+	})
+
+	defer func() {
+		if t.Failed() {
+			apmtest.WriteTraceWaterfall(os.Stdout, tx, spans)
+		}
+	}()
+
+	assert.NotNil(t, tx)
+	assert.Equal(t, 2, len(spans))
+
+	mysqlSpan := spans[0]
+	assert.Equal(t, mysqlSpan.Context.Destination.Service.Resource, "mysql")
+	assert.Nil(t, mysqlSpan.Composite)
+
+	requestSpan := spans[1]
+	assert.Equal(t, requestSpan.Context.Destination.Service.Resource, "request")
+	assert.NotNil(t, requestSpan.Composite)
+	assert.Equal(t, requestSpan.Composite.Count, 5)
+	assert.Equal(t, requestSpan.Name, "Calls to request")
+	// Check that the aggregate sum is at least the duration of the time we
+	// we waited for.
+	assert.Greater(t, requestSpan.Composite.Sum, float64(5*100/time.Millisecond))
+
+	// Check that the total composite span duration is at least 5 milliseconds.
+	assert.Greater(t, requestSpan.Duration, float64(5*100/time.Millisecond))
+}
+
+func TestCompressSpanSameKindParentSpan(t *testing.T) {
+	tracer := apmtest.NewRecordingTracer()
+	tracer.SetSpanCompressionEnabled(true)
+
+	// This test case covers spans that have other spans as parents.
+	tx, spans, _ := tracer.WithTransaction(func(ctx context.Context) {
+		{
+			// Doesn't compress any spans since none meet the necessary conditions
+			// the "request" type are both the same type but the parent
+			// [       Transaction       ...snip
+			//  [       internal op     ]
+			//   [        request      ]
+			//          [   request   ]
+			//
+			parent, ctx := apm.StartSpan(ctx, "internal op", "internal")
+			// Have span propagate context downstream, this should not allow for
+			// compression
+			child, ctx := apm.StartSpan(ctx, "GET /resource", "request")
+			grandChild, _ := apm.StartSpanOptions(ctx, "GET /different", "request", apm.SpanOptions{
+				ExitSpan: true,
+				Parent:   child.TraceContext(),
+			})
+			<-time.After(500 * time.Nanosecond)
+			grandChild.End()
+			child.End()
+			parent.End()
+		}
+		{
+			// Compresses the last two spans together since they are both exit
+			// spans, same "request" type, don't propagate ctx and succeed.
+			// ..continued  Transaction   ]
+			//  [       internal op     ]
+			//       [  request (2)   ]  ( [GET /res] [GET /diff] )
+			//
+			exitSpanOpts := apm.SpanOptions{ExitSpan: true}
+			parent, ctx := apm.StartSpan(ctx, "another op", "internal")
+			child, _ := apm.StartSpanOptions(ctx, "GET /res", "request", exitSpanOpts)
+			<-time.After(300 * time.Nanosecond)
+
+			otherChild, _ := apm.StartSpanOptions(ctx, "GET /diff", "request", exitSpanOpts)
+			<-time.After(300 * time.Nanosecond)
+
+			otherChild.End()
+			child.End()
+
+			parent.End()
+		}
+	})
+
+	defer func() {
+		if t.Failed() {
+			apmtest.WriteTraceWaterfall(os.Stdout, tx, spans)
+		}
+	}()
+	assert.NotNil(t, tx)
+	assert.Equal(t, 5, len(spans))
+
+	// Since the spans are started very close together, even a time.Sleep
+	// doesn't return the spans in a deterministic order, which is OK.
+	var compositeSpan model.Span
+	var compositeParent model.Span
+	for _, span := range spans {
+		if span.Name == "another op" && span.Type == "internal" {
+			compositeParent = span
+		}
+	}
+	for _, span := range spans {
+		if span.Composite != nil {
+			compositeSpan = span
+			assert.Equal(t, "Calls to request", span.Name)
+			assert.Equal(t, "request", span.Type)
+		}
+	}
+
+	assert.NotNil(t, compositeSpan)
+	assert.NotNil(t, compositeSpan.Composite)
+	assert.Equal(t, compositeSpan.Composite.Count, 2)
+	assert.Equal(t, compositeSpan.ParentID, compositeParent.ID)
+	assert.GreaterOrEqual(t, compositeParent.Duration, compositeSpan.Duration)
 }
 
 func TestExitSpanDoesNotOverwriteDestinationServiceResource(t *testing.T) {
