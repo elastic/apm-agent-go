7533cda5bbc70b85515e284f1de3d4dd09e42eeb
Sergio Andres Virviescas Santana <savsgio.engineer@gmail.com>
2022-01-24 10:08:31 +0100

    tests: improvements

--- module/apmfasthttp/server_test.go
+++ module/apmfasthttp/server_test.go
@@ -23,6 +23,7 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 
@@ -35,7 +36,9 @@ import (
 	"go.elastic.co/apm/transport/transporttest"
 )
 
-func testServer(t *testing.T, s *fasthttp.Server, assertFunc func(model.Transaction, *http.Response)) {
+type assertFunc func(model.Transaction, *http.Response)
+
+func testServer(t *testing.T, s *fasthttp.Server, wg *sync.WaitGroup, assertFn assertFunc) {
 	t.Helper()
 
 	tracer, transport := transporttest.NewRecorderTracer()
@@ -43,6 +46,7 @@ func testServer(t *testing.T, s *fasthttp.Server, assertFunc func(model.Transact
 
 	shutdown := make(chan error)
 	defer close(shutdown)
+
 	ln, err := net.Listen("tcp", "127.0.0.1:")
 	require.NoError(t, err)
 
@@ -53,35 +57,39 @@ func testServer(t *testing.T, s *fasthttp.Server, assertFunc func(model.Transact
 	}()
 
 	addr := ln.Addr().String()
+
 	resp, err := http.Get("http://" + addr)
 	require.NoError(t, err)
-	io.Copy(ioutil.Discard, resp.Body) // consume body to complete request handler
+
+	_, _ = io.Copy(ioutil.Discard, resp.Body) // consume body to complete request handler
 	resp.Body.Close()
+
+	if wg != nil {
+		wg.Wait()
+	}
+
 	tracer.Flush(nil)
 	payloads := transport.Payloads()
 
-	// the transaction is ended after the response body is fully written,
-	// so a single call to `tracer.Flush()` may not have an event yet
-	// enqueued. Continue to call `tracer.Flush()` and wait for the
-	// transaction to have ended.
-	// This is unique to fasthttp's implementation.
-	timer := time.NewTimer(200 * time.Millisecond)
-	defer timer.Stop()
-	for {
-		select {
-		case <-timer.C:
-			t.Fatal("timed out waiting for payload")
-		default:
+	count := 0
+	for len(payloads.Transactions) == 0 {
+		// the transaction is ended after the response body is fully written,
+		// so a single call to `tracer.Flush()` may not have an event yet
+		// enqueued. Continue to call `tracer.Flush()` and wait for the
+		// transaction to have ended.
+		// This is unique to fasthttp's implementation.
+		if count > 10 {
+			t.Fatal("no transaction found")
 		}
-		if len(payloads.Transactions) > 0 {
-			break
-		}
-		time.Sleep(10 * time.Millisecond)
+
+		count++
+
+		time.Sleep(100 * time.Millisecond)
 		tracer.Flush(nil)
 		payloads = transport.Payloads()
 	}
 
-	assertFunc(payloads.Transactions[0], resp)
+	assertFn(payloads.Transactions[0], resp)
 
 	// s.Serve returns on ln.Close()
 	ln.Close()
@@ -96,7 +104,7 @@ func TestServerHTTPResponse(t *testing.T) {
 		Name: "test-server",
 	}
 
-	assertFunc := func(transaction model.Transaction, resp *http.Response) {
+	assertFn := func(transaction model.Transaction, resp *http.Response) {
 		expectedHeaders := model.Headers{
 			{Key: "Content-Length", Values: []string{"12"}},
 			{Key: "Content-Type", Values: []string{"text/plain; charset=utf-8"}},
@@ -104,13 +112,14 @@ func TestServerHTTPResponse(t *testing.T) {
 		}
 
 		assert.Equal(t, fasthttp.StatusUnauthorized, resp.StatusCode)
+		assert.Equal(t, resp.StatusCode, transaction.Context.Response.StatusCode)
 		assert.Equal(t, "GET /", transaction.Name)
 		assert.Equal(t, "request", transaction.Type)
 		assert.Equal(t, "HTTP 4xx", transaction.Result)
 		assert.Equal(t, expectedHeaders, transaction.Context.Response.Headers)
 	}
 
-	testServer(t, s, assertFunc)
+	testServer(t, s, nil, assertFn)
 }
 
 func TestServerHTTPResponseStream(t *testing.T) {
@@ -126,7 +135,7 @@ func TestServerHTTPResponseStream(t *testing.T) {
 		Name: "test-server",
 	}
 
-	assertFunc := func(transaction model.Transaction, resp *http.Response) {
+	assertFn := func(transaction model.Transaction, resp *http.Response) {
 		expectedHeaders := model.Headers{
 			{Key: "Content-Type", Values: []string{"text/plain; charset=utf-8"}},
 			{Key: "Server", Values: []string{s.Name}},
@@ -134,6 +143,7 @@ func TestServerHTTPResponseStream(t *testing.T) {
 		}
 
 		assert.Equal(t, fasthttp.StatusOK, resp.StatusCode)
+		assert.Equal(t, resp.StatusCode, transaction.Context.Response.StatusCode)
 		assert.Equal(t, "GET /", transaction.Name)
 		assert.Equal(t, "request", transaction.Type)
 		assert.Equal(t, "HTTP 2xx", transaction.Result)
@@ -141,22 +151,26 @@ func TestServerHTTPResponseStream(t *testing.T) {
 		assert.Equal(t, transaction.Context.Response.Headers, expectedHeaders)
 	}
 
-	testServer(t, s, assertFunc)
+	testServer(t, s, nil, assertFn)
 }
 
 func TestServerHTTPResponseHijack(t *testing.T) {
 	hijackResponseDuration := time.Millisecond * 100
 
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
 	s := &fasthttp.Server{
 		Handler: func(ctx *fasthttp.RequestCtx) {
 			ctx.Hijack(func(c net.Conn) {
 				time.Sleep(hijackResponseDuration)
+				wg.Done()
 			})
 		},
 		Name: "test-server",
 	}
 
-	assertFunc := func(transaction model.Transaction, resp *http.Response) {
+	assertFn := func(transaction model.Transaction, resp *http.Response) {
 		expectedHeaders := model.Headers{
 			{Key: "Content-Length", Values: []string{"0"}},
 			{Key: "Content-Type", Values: []string{"text/plain; charset=utf-8"}},
@@ -164,6 +178,7 @@ func TestServerHTTPResponseHijack(t *testing.T) {
 		}
 
 		assert.Equal(t, fasthttp.StatusOK, resp.StatusCode)
+		assert.Equal(t, resp.StatusCode, transaction.Context.Response.StatusCode)
 		assert.Equal(t, "GET /", transaction.Name)
 		assert.Equal(t, "request", transaction.Type)
 		assert.Equal(t, "HTTP 2xx", transaction.Result)
@@ -171,5 +186,5 @@ func TestServerHTTPResponseHijack(t *testing.T) {
 		assert.Equal(t, transaction.Context.Response.Headers, expectedHeaders)
 	}
 
-	testServer(t, s, assertFunc)
+	testServer(t, s, wg, assertFn)
 }
