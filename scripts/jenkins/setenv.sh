60edddc7c65ce62df651afb7ac31499f83b54756
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-14 21:53:44 +0000

    split go installation from tools

--- scripts/jenkins/setenv.sh
+++ scripts/jenkins/setenv.sh
@@ -5,8 +5,4 @@ set -euxo pipefail
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
-# Install tools used only in CI using a local go.mod file.
-GO_GET_FLAGS="-modfile=$PWD/scripts/jenkins/jenkins.go.mod"
-
-go get $GO_GET_FLAGS -v github.com/jstemmer/go-junit-report
-go get $GO_GET_FLAGS -v github.com/t-yuki/gocover-cobertura
+source ./scripts/jenkins/setenv-tools.sh
