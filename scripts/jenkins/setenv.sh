6e41fe31feda8950061673a822f6999ccc5e8f81
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-15 20:35:23 +0000

    more debug

--- scripts/jenkins/setenv.sh
+++ scripts/jenkins/setenv.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 which go || true
 whereis go || true
 env | sort
+ls -ltra ls -l /usr/local/go || true
+ls -l /usr/local/go/bin || true
 
 # Install tools used only in CI using a local go.mod file.
 GO_GET_FLAGS="-modfile=$PWD/scripts/jenkins/jenkins.go.mod"
