2db48bb376d5ba6ac5a1703f4aa7773e8ddbc546
K.Gvencadze <k.gvencadze@icloud.com>
2022-09-15 23:29:43 +0300

    docs(ci): add license header, run update-licenses,update-modules, fmt and add apmpgx entry to Dockerfile-testing

--- module/apmpgx/go.mod
+++ module/apmpgx/go.mod
@@ -1,12 +1,14 @@
 module go.elastic.co/apm/module/apmpgx/v2
 
-go 1.18
+go 1.15
 
 require (
 	github.com/jackc/pgx/v4 v4.17.0
 	github.com/stretchr/testify v1.8.0
-	go.elastic.co/apm/module/apmsql/v2 v2.0.0
+	go.elastic.co/apm/module/apmsql/v2 v2.1.0
 	go.elastic.co/apm/v2 v2.1.0
 )
 
 replace go.elastic.co/apm/v2 => ../..
+
+replace go.elastic.co/apm/module/apmsql/v2 => ../apmsql
--- module/apmpgx/tracer_test.go
+++ module/apmpgx/tracer_test.go
@@ -1,15 +1,34 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package apmpgx_test
 
 import (
 	"context"
 	"errors"
+	"testing"
+	"time"
+
 	"github.com/jackc/pgx/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.elastic.co/apm/module/apmpgx/v2"
 	"go.elastic.co/apm/v2/apmtest"
-	"testing"
-	"time"
 )
 
 type testLogger struct{}
