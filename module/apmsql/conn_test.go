b98fc85bec3f0d8eae0270302e433868b06022f2
Bong Nguyen <bong.nguyen@safetyculture.io>
2022-10-28 13:39:36 +1100

    Add test for conn.Unwrap

---
+++ module/apmsql/conn_test.go
@@ -0,0 +1,31 @@
+package apmsql_test
+
+import (
+	"context"
+	"database/sql/driver"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"go.elastic.co/apm/module/apmsql"
+	_ "go.elastic.co/apm/module/apmsql/sqlite3"
+)
+
+func TestConnUnwrap(t *testing.T) {
+	db, err := apmsql.Open("sqlite3", ":memory:")
+	require.NoError(t, err)
+	defer db.Close()
+
+	type Unwrapper interface {
+		Unwrap() driver.Conn
+	}
+
+	ctx := context.Background()
+	conn, err := db.Conn(ctx)
+	_ = conn.Raw(func(driverConn interface{}) error {
+		unwrappedConn := driverConn.(Unwrapper)
+		require.Equal(t, "*apmsql.connBeginTx", fmt.Sprintf("%T", unwrappedConn))
+		return nil
+	})
+}
