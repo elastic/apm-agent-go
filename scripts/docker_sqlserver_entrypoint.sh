afdcc7f0678fdce4bf03f02dada2a15a1021127f
Jamess-Lucass <jameslucas1210@gmail.com>
2022-12-01 21:22:14 +0000

    ci: Retry sqlcmd for 90s while creating sqlserver

---
+++ scripts/docker_sqlserver_entrypoint.sh
@@ -0,0 +1,13 @@
+# SQL Server can take a while to start, this scripts waits until it's ready and then creates a database
+for i in {1..90};
+do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123 -d master -i "IF DB_ID('test_db') IS NULL CREATE DATABASE test_db"
+    if [ $? -eq 0 ]
+    then
+        echo "setup complete"
+        break
+    else
+        echo "not ready yet..."
+        sleep 1
+    fi
+done
