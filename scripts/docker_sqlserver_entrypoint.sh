39eec41665e60dfc183163e0721aec6c7a67a9f5
Jamess-Lucass <jameslucas1210@gmail.com>
2022-12-03 21:45:33 +0000

    fix: Fixed sqlcmd query

--- scripts/docker_sqlserver_entrypoint.sh
+++ scripts/docker_sqlserver_entrypoint.sh
@@ -1,7 +1,7 @@
 # SQL Server can take a while to start, this scripts waits until it's ready and then creates a database
 for i in {1..90};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123 -d master -i "IF DB_ID('test_db') IS NULL CREATE DATABASE test_db"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123 -d master -q "IF DB_ID('test_db') IS NULL CREATE DATABASE test_db"
     if [ $? -eq 0 ]
     then
         echo "setup complete"
