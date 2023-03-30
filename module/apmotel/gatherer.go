16e6739a8de8fe4e5623a65bf86efb7d6016b2d8
dmathieu <damien.mathieu@elastic.co>
2023-03-30 13:34:01 +0200

    bootstrap apmotel module
    Signed-off-by: dmathieu <damien.mathieu@elastic.co>

---
+++ module/apmotel/gatherer.go
@@ -0,0 +1,83 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package apmotel // import "go.elastic.co/apm/module/apmotel"
+
+import (
+	"context"
+
+	"go.elastic.co/apm"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+)
+
+// NewGatherer creates a new gatherer/exporter to bridge between agent metrics and OpenTelemetry
+func NewGatherer() (Gatherer, error) {
+	reader := metric.NewManualReader()
+
+	return Gatherer{reader}, nil
+}
+
+// Gatherer is a gatherer/exporter which can act as an OpenTelemetry manual
+// reader, to retrieve metrics and expose them to the agent.
+type Gatherer struct {
+	metric.Reader
+}
+
+var _ metric.Reader = Gatherer{}
+var _ apm.MetricsGatherer = Gatherer{}
+
+// GatherMEtrics gathers metrics into out.
+func (e Gatherer) GatherMetrics(ctx context.Context, out *apm.Metrics) error {
+	metrics := metricdata.ResourceMetrics{}
+	err := e.Reader.Collect(ctx, &metrics)
+	if err != nil {
+		return err
+	}
+
+	for _, scopeMetrics := range metrics.ScopeMetrics {
+
+		for _, sm := range scopeMetrics.Metrics {
+			switch m := sm.Data.(type) {
+			case metricdata.Histogram[int64]:
+				// Histogram
+			case metricdata.Histogram[float64]:
+				// Histogram
+			case metricdata.Sum[int64]:
+				for _, dp := range m.DataPoints {
+					out.Add(sm.Name, makeLabels(dp.Attributes), float64(dp.Value))
+				}
+			case metricdata.Sum[float64]:
+				for _, dp := range m.DataPoints {
+					out.Add(sm.Name, makeLabels(dp.Attributes), dp.Value)
+				}
+			case metricdata.Gauge[int64]:
+				// Gauge
+			case metricdata.Gauge[float64]:
+				// Gauge
+			}
+		}
+	}
+
+	return nil
+}
+
+func makeLabels(attrs attribute.Set) []apm.MetricLabel {
+	labels := make([]apm.MetricLabel, 0)
+	return labels
+}
---
+++ module/apmotel/gatherer_test.go
@@ -0,0 +1,94 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package apmotel
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+
+	"go.elastic.co/apm"
+	"go.elastic.co/apm/apmtest"
+	"go.elastic.co/apm/model"
+)
+
+func TestGatherer(t *testing.T) {
+	for _, tt := range []struct {
+		name string
+
+		recordMetrics   func(ctx context.Context, meter metric.Meter)
+		expectedMetrics []model.Metrics
+	}{
+		{
+			name: "with a float64 counter",
+			recordMetrics: func(ctx context.Context, meter metric.Meter) {
+				counter, err := meter.Float64Counter("foo")
+				assert.NoError(t, err)
+				counter.Add(ctx, 5)
+			},
+			expectedMetrics: []model.Metrics{
+				{
+					Samples: map[string]model.Metric{
+						"foo": {
+							Value: 5,
+						},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			gatherer, err := NewGatherer()
+			assert.NoError(t, err)
+			provider := sdkmetric.NewMeterProvider(sdkmetric.WithReader(gatherer))
+			meter := provider.Meter("apmotel_test")
+
+			tt.recordMetrics(ctx, meter)
+
+			metrics := gatherMetrics(gatherer)
+
+			// Remove all internal metrics
+			for k := range metrics[0].Samples {
+				if strings.HasPrefix(k, "golang.") {
+					delete(metrics[0].Samples, k)
+				}
+			}
+
+			assert.Equal(t, tt.expectedMetrics, metrics)
+		})
+	}
+}
+
+func gatherMetrics(g apm.MetricsGatherer) []model.Metrics {
+	tracer := apmtest.NewRecordingTracer()
+	defer tracer.Close()
+	tracer.RegisterMetricsGatherer(g)
+	tracer.SendMetrics(nil)
+	metrics := tracer.Payloads().Metrics
+	for i := range metrics {
+		metrics[i].Timestamp = model.Time{}
+	}
+	return metrics
+}
