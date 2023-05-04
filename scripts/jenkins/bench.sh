c5319e55f900eaf2cd307d93f35224e825e4cbc9
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-05-04 08:45:55 +0200

    configure for buildkite explicitly

--- scripts/jenkins/bench.sh
+++ scripts/jenkins/bench.sh
@@ -2,24 +2,26 @@
 set -exo pipefail
 
 ## Buildkite specific configuration
-export GO111MODULE=on
+if [ "$CI" == "true" ] ; then
+	export GO111MODULE=on
 
-# If HOME is not set then use the current directory
-# that's normally happening when running in the CI
-# owned by Elastic.
-if [ -z "$HOME" ] ; then
-	HOME=$(realpath ~)
-	export HOME
+	# If HOME is not set then use the current directory
+	# that's normally happening when running in the CI
+	# owned by Elastic.
+	if [ -z "$HOME" ] ; then
+		HOME=$(realpath ~)
+		export HOME
+	fi
+
+	# Make sure gomod can be deleted automatically as part of the CI
+	clean_up () {
+	  ARG=$?
+	  # see https://github.com/golang/go/issues/31481#issuecomment-485008558
+	  chmod u+w -R $HOME
+	  exit $ARG
+	}
+	trap clean_up EXIT
 fi
-
-# Make sure gomod can be deleted automatically as part of the CI
-clean_up () {
-  ARG=$?
-  # see https://github.com/golang/go/issues/31481#issuecomment-485008558
-  chmod u+w -R $GOPATH/pkg/mod
-  exit $ARG
-}
-trap clean_up EXIT
 
 ## Bench specific
 set -u
