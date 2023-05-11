fc03110a462eefc76a038aafbd30e27d1987f39e
kruskall <99559985+kruskall@users.noreply.github.com>
2023-05-11 21:10:36 +0200

    feat: drop x/net dependency (#1434)
    * feat: drop x/net dependency
    
    * build: run go mod tidy

--- module/apmelasticsearch/go.mod
+++ module/apmelasticsearch/go.mod
@@ -4,7 +4,6 @@ require (
 	github.com/stretchr/testify v1.6.1
 	go.elastic.co/apm/module/apmhttp/v2 v2.4.1
 	go.elastic.co/apm/v2 v2.4.1
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f
 )
 
 replace go.elastic.co/apm/v2 => ../..
--- module/apmhttp/client_bench_test.go
+++ module/apmhttp/client_bench_test.go
@@ -24,7 +24,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"golang.org/x/net/context/ctxhttp"
 
 	"go.elastic.co/apm/module/apmhttp/v2"
 	"go.elastic.co/apm/v2"
@@ -55,7 +54,9 @@ func benchmarkClient(b *testing.B, wrap func(*http.Client) *http.Client) {
 			client := wrap(nil)
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
-				resp, err := ctxhttp.Get(ctx, client, server.URL+path)
+				req, err := http.NewRequestWithContext(ctx, http.MethodGet, server.URL+path, nil)
+				require.NoError(b, err)
+				resp, err := client.Do(req)
 				require.NoError(b, err)
 				resp.Body.Close()
 			}
