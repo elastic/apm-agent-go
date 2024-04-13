09e015c95d27d7cd13d64c5f009098381022bfba
cmenke <cmenke@remax.com>
2024-04-13 14:16:56 -0600

    Multi Error Reporting, Custom Report Attrs, and Clean Up
    Implemented ability to report multiple apm errors from one log. If a user adds
    multiple "reportable" error attributes to the log msg (default is "error" & "err"),
    instead of trying to join the errors into one or discarding one, the apmslog
    handler will report both errors.
    
    Added ability for a user to define what slog attribute keys they want to report
    as errors. Because there is no standard way in slog to attach an error to a msg
    log, I wanted to add the ability for the user to decide what is and what is not
    going to be reported. By default, slog attribute keys that are "error" or "err"
    are reported, but with the new `WithErrorRecordAttrs(keys)` function a user
    can define which keys will be reported.
    
    Cleaned up `ApmHandler` struct and methods. Since we want the user to use the
    included `NewApmHandler` function and its functional option functions, I
    decided to make all Struct fields private.
    
    Additionally added a check on if the `ApmHandler`'s `tracer` field is nill before
    trying to use it. It is still possible for a user to pass in a nil tracer using
    the `WithTracer` functional option.
    
    New tests and documentation added.

--- module/apmslog/handler.go
+++ module/apmslog/handler.go
@@ -45,35 +45,28 @@ const (
 )
 
 type ApmHandler struct {
-	Tracer       *apm.Tracer
-	ReportLevels []slog.Level
-	Handler      slog.Handler
-}
-
-func (h *ApmHandler) tracer() *apm.Tracer {
-	if h.Tracer == nil {
-		return apm.DefaultTracer()
-	}
-	return h.Tracer
+	tracer           *apm.Tracer
+	reportLevels     []slog.Level
+	errorRecordAttrs []string
+	handler          slog.Handler
 }
 
 // Enabled reports whether the handler handles records at the given level.
 func (h *ApmHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return h.Handler.Enabled(ctx, level)
+	return h.handler.Enabled(ctx, level)
 }
 
 // WithAttrs returns a new ApmHandler with passed attributes attached.
 func (h *ApmHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &ApmHandler{h.Tracer, h.ReportLevels, h.Handler.WithAttrs(attrs)}
+	return &ApmHandler{h.tracer, h.reportLevels, h.errorRecordAttrs, h.handler.WithAttrs(attrs)}
 }
 
 // WithGroup returns a new ApmHandler with passed group attached.
 func (h *ApmHandler) WithGroup(name string) slog.Handler {
-	return &ApmHandler{h.Tracer, h.ReportLevels, h.Handler.WithGroup(name)}
+	return &ApmHandler{h.tracer, h.reportLevels, h.errorRecordAttrs, h.handler.WithGroup(name)}
 }
 
 func (h *ApmHandler) Handle(ctx context.Context, r slog.Record) error {
-
 	// attempt to extract any available trace info from context
 	var traceId apm.TraceID
 	var transactionId apm.SpanID
@@ -93,60 +86,83 @@ func (h *ApmHandler) Handle(ctx context.Context, r slog.Record) error {
 	}
 
 	// report record as APM error
-	tracer := h.tracer()
-	if slices.Contains(h.ReportLevels, r.Level) && tracer.Recording() {
+	if h.tracer != nil && h.tracer.Recording() && slices.Contains(h.reportLevels, r.Level) {
 
-		// attempt to find error/err attribute
+		// attempt to find error attributes
 		// slog doesnt have a standard way of attaching an
 		// error to a record, so attempting to grab any attribute
-		// that has error/err as key and extracting the value
-		// seems like a likely way to do it.
-		var err error
+		// that has error/err keys OR keys user has defined as reportable
+		// and extracting the values seems like a likely way to do it.
+		errorsToAttach := []error{}
 		r.Attrs(func(a slog.Attr) bool {
-			if a.Key == SlogErrorKeyErr || a.Key == SlogErrorKeyError {
+			if slices.Contains(h.errorRecordAttrs, a.Key) {
+				var err error
+				// first check if value is of error type to retain as much info as possible
 				if v, ok := a.Value.Any().(error); ok {
-					err = v
-					return false
+					errorsToAttach = append(errorsToAttach, v)
+					// else just convert reportable error value as string
 				} else {
-					err = errors.Join(err, fmt.Errorf("%s", a.Value.String()))
-					return false
+					errorsToAttach = append(errorsToAttach, errors.Join(err, fmt.Errorf("%s", a.Value.String())))
 				}
 			}
 			return true
 		})
 
-		errlog := tracer.NewErrorLog(apm.ErrorLogRecord{
-			Message: r.Message,
-			Level:   strings.ToLower(r.Level.String()),
-			Error:   err,
-		})
-		errlog.Handled = true
-		errlog.Timestamp = r.Time.UTC()
-		errlog.SetStacktrace(2)
+		// If there are multiple reportable error attributes, create a new
+		// apm.ErrorLogRecord for each. Otherwise just create one apm.ErrorLogRecord
+		// with no Error.
+		errLogRecords := []apm.ErrorLogRecord{}
+		if len(errorsToAttach) == 0 {
+			errRecord := apm.ErrorLogRecord{
+				Message: r.Message,
+				Level:   strings.ToLower(r.Level.String()),
+			}
+			errLogRecords = append(errLogRecords, errRecord)
+		} else {
+			for _, err := range errorsToAttach {
+				errRecord := apm.ErrorLogRecord{
+					Message: r.Message,
+					Level:   strings.ToLower(r.Level.String()),
+					Error:   err,
+				}
+				errLogRecords = append(errLogRecords, errRecord)
+			}
+		}
 
-		// add available trace info if not zero type
-		if traceId != (apm.TraceID{}) {
-			errlog.TraceID = traceId
+		// for each errRecord, send to apm
+		for _, errRecord := range errLogRecords {
+			errlog := h.tracer.NewErrorLog(errRecord)
+			errlog.Handled = true
+			errlog.Timestamp = r.Time.UTC()
+			errlog.SetStacktrace(2)
+
+			// add available trace info if not zero type
+			if traceId != (apm.TraceID{}) {
+				errlog.TraceID = traceId
+			}
+			if transactionId != (apm.SpanID{}) {
+				errlog.TransactionID = transactionId
+			}
+			if parentId != (apm.SpanID{}) {
+				errlog.ParentID = parentId
+			}
+			// send error to APM
+			errlog.Send()
+
 		}
-		if transactionId != (apm.SpanID{}) {
-			errlog.TransactionID = transactionId
-		}
-		if parentId != (apm.SpanID{}) {
-			errlog.ParentID = parentId
-		}
-		// send error to APM
-		errlog.Send()
 	}
 
-	return h.Handler.Handle(ctx, r)
+	return h.handler.Handle(ctx, r)
 }
 
 type apmHandlerOption func(h *ApmHandler)
 
+// Create a new ApmHandler.
 func NewApmHandler(opts ...apmHandlerOption) *ApmHandler {
 	h := &ApmHandler{
 		apm.DefaultTracer(),
 		[]slog.Level{slog.LevelError},
+		[]string{SlogErrorKeyErr, SlogErrorKeyError},
 		slog.Default().Handler(),
 	}
 	for _, opt := range opts {
@@ -155,20 +171,34 @@ func NewApmHandler(opts ...apmHandlerOption) *ApmHandler {
 	return h
 }
 
+// Set slog handler for ApmHandler
+// default: slog.Default().Handler()
 func WithHandler(handler slog.Handler) apmHandlerOption {
 	return func(h *ApmHandler) {
-		h.Handler = handler
+		h.handler = handler
 	}
 }
 
+// Set which slog log level will be reported
+// default: slog.LevelError
 func WithReportLevel(lvls []slog.Level) apmHandlerOption {
 	return func(h *ApmHandler) {
-		h.ReportLevels = lvls
+		h.reportLevels = lvls
 	}
 }
 
+// Set with slog attribute keys will be used as errors.
+// default: 'error','err'
+func WithErrorRecordAttrs(keys []string) apmHandlerOption {
+	return func(h *ApmHandler) {
+		h.errorRecordAttrs = keys
+	}
+}
+
+// Set custom tracer for ApmHandler.
+// default: apm.DefaultTracer()
 func WithTracer(tracer *apm.Tracer) apmHandlerOption {
 	return func(h *ApmHandler) {
-		h.Tracer = tracer
+		h.tracer = tracer
 	}
 }
--- module/apmslog/handler_test.go
+++ module/apmslog/handler_test.go
@@ -20,6 +20,8 @@ package apmslog_test
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log/slog"
 	"testing"
@@ -33,40 +35,37 @@ import (
 	"go.elastic.co/apm/v2/transport/transporttest"
 )
 
-func TestHandler(t *testing.T) {
+// it should add no trace attributes and not report an error
+func TestHandlerNoContext(t *testing.T) {
 	tracer, transport := transporttest.NewRecorderTracer()
 	defer tracer.Close()
 	var buf bytes.Buffer
-
 	h := newApmslogHandler(&buf, tracer)
 	logger := slog.New(h)
 
-	logger.Error("hello world", "error", errors.New("new error"))
+	logger.Info("hello world")
 
-	assert.Equal(t, `{"time":"1970-01-01T00:00:00Z","level":"ERROR","msg":"hello world","error":"new error"}`+"\n", buf.String())
+	// assert msg looks as expected
+	assert.Equal(t, `{"time":"1970-01-01T00:00:00Z","level":"INFO","msg":"hello world"}`+"\n", buf.String())
 
 	tracer.Flush(nil)
 	payloads := transport.Payloads()
-	assert.Len(t, payloads.Errors, 1)
-
-	err0 := payloads.Errors[0]
-	assert.Equal(t, "hello world", err0.Log.Message)
-	assert.Equal(t, "error", err0.Log.Level)
-	assert.Equal(t, "", err0.Log.LoggerName)
-	assert.Equal(t, "", err0.Log.ParamMessage)
-	assert.Equal(t, "TestHandler", err0.Culprit)
-	assert.NotEmpty(t, err0.Log.Stacktrace)
-	// assert.Equal(t, model.Time(time.Unix(0, 0).UTC()), err0.Timestamp) // seems like slog time attribute is not accessable
-	assert.Zero(t, err0.ParentID)
-	assert.Zero(t, err0.TraceID)
-	assert.Zero(t, err0.TransactionID)
+	// assert we reported no errors to apm
+	assert.Len(t, payloads.Errors, 0)
 }
-func TestHandlerTransactionTraceContext(t *testing.T) {
+
+// Helper struct to check msg traces
+type MsgWithTrace struct {
+	TransactionId string `json:"transaction.id"`
+	TraceId       string `json:"trace.id"`
+	SpanId        string `json:"span.id"`
+}
+
+// it should add trace attributes and not report an error
+func TestHandlerWithContext(t *testing.T) {
 	tracer, transport := transporttest.NewRecorderTracer()
 	defer tracer.Close()
-
 	var buf bytes.Buffer
-
 	h := newApmslogHandler(&buf, tracer)
 	logger := slog.New(h)
 
@@ -74,53 +73,263 @@ func TestHandlerTransactionTraceContext(t *testing.T) {
 	ctx := apm.ContextWithTransaction(context.Background(), tx)
 	span, ctx := apm.StartSpan(ctx, "name", "type")
 
-	logger.ErrorContext(ctx, "hello world", "error", errors.New("new error"))
+	logger.InfoContext(ctx, "hello world")
 
 	span.End()
 	tx.End()
 
+	var msg MsgWithTrace
+	err := json.Unmarshal([]byte(buf.String()), &msg)
+    assert.NoError(t, err)
+
+	// assert that we added the correct traces to buff
+	assert.Equal(t, tx.TraceContext().Trace.String(), msg.TraceId)
+	assert.Equal(t, span.TraceContext().Span.String(), msg.SpanId)
+	assert.Equal(t, tx.TraceContext().Span.String(), msg.TransactionId)
+
+	// assert msg is as expected
+	assert.Equal(t,
+		fmt.Sprintf(`{"time":"1970-01-01T00:00:00Z","level":"INFO","msg":"hello world","trace.id":"%s","transaction.id":"%s","span.id":"%s"}`,
+			msg.TraceId,
+			msg.TransactionId,
+			msg.SpanId,
+		)+"\n",
+		buf.String(),
+	)
+
+	// assert we reported no errors to apm
+	tracer.Flush(nil)
+	payloads := transport.Payloads()
+	assert.Len(t, payloads.Errors, 0)
+}
+
+// it should add no trace attributes but still report an error
+func TestHandlerErrorNoContext(t *testing.T) {
+	tracer, transport := transporttest.NewRecorderTracer()
+	defer tracer.Close()
+	var buf bytes.Buffer
+	h := newApmslogHandler(&buf, tracer)
+	logger := slog.New(h)
+
+	logger.Error("hello world", "error", errors.New("new error"))
+
+	// assert msg looks as expected
+	assert.Equal(t, `{"time":"1970-01-01T00:00:00Z","level":"ERROR","msg":"hello world","error":"new error"}`+"\n", buf.String())
+
+	// assert we reported one error to apm
+	tracer.Flush(nil)
+	payloads := transport.Payloads()
+	assert.Len(t, payloads.Errors, 1)
+	assert.Empty(t, payloads.Transactions, 0)
+	assert.Empty(t, payloads.Spans, 0)
+
+	// assert apm error fields are correct
+	err0 := payloads.Errors[0]
+	assert.Equal(t, "hello world", err0.Log.Message)
+	assert.Equal(t, "error", err0.Log.Level)
+	assert.Equal(t, "", err0.Log.LoggerName)
+	assert.Equal(t, "", err0.Log.ParamMessage)
+	assert.Equal(t, "TestHandlerErrorNoContext", err0.Culprit)
+	assert.NotEmpty(t, err0.Log.Stacktrace)
+	assert.Zero(t, err0.ParentID)
+	assert.Zero(t, err0.TraceID)
+	assert.Zero(t, err0.TransactionID)
+}
+
+// mock function to test apm error fields
+func mockFuncError() error {
+	return errors.New("new error")
+}
+
+// it should add trace attributes and report an error
+func TestHandlerErrorWithContext(t *testing.T) {
+	tracer, transport := transporttest.NewRecorderTracer()
+	defer tracer.Close()
+	var buf bytes.Buffer
+	h := newApmslogHandler(&buf, tracer)
+	logger := slog.New(h)
+
+	// create transactions and spans
+	tx := tracer.StartTransaction("name", "type")
+	ctx := apm.ContextWithTransaction(context.Background(), tx)
+	span, ctx := apm.StartSpan(ctx, "name", "type")
+
+	logger.ErrorContext(ctx, "hello world", "error", mockFuncError())
+
+	span.End()
+	tx.End()
+
+	var msg MsgWithTrace
+	err := json.Unmarshal([]byte(buf.String()), &msg)
+    assert.NoError(t, err)
+
+	// assert that we added the correct traces to buff
+	assert.Equal(t, tx.TraceContext().Trace.String(), msg.TraceId)
+	assert.Equal(t, span.TraceContext().Span.String(), msg.SpanId)
+	assert.Equal(t, tx.TraceContext().Span.String(), msg.TransactionId)
+
+	// assert msg is as expected
+	assert.Equal(t,
+		fmt.Sprintf(`{"time":"1970-01-01T00:00:00Z","level":"ERROR","msg":"hello world","error":"new error","trace.id":"%s","transaction.id":"%s","span.id":"%s"}`,
+			msg.TraceId,
+			msg.TransactionId,
+			msg.SpanId,
+		)+"\n",
+		buf.String(),
+	)
+
+	// assert we reported one error with traces to apm
 	tracer.Flush(nil)
 	payloads := transport.Payloads()
 	assert.Len(t, payloads.Transactions, 1)
 	assert.Len(t, payloads.Spans, 1)
 	assert.Len(t, payloads.Errors, 1)
 
+	// assert apm error fields are correct
 	err0 := payloads.Errors[0]
 	assert.Equal(t, payloads.Spans[0].ID, err0.ParentID)
 	assert.Equal(t, payloads.Transactions[0].TraceID, err0.TraceID)
 	assert.Equal(t, payloads.Transactions[0].ID, err0.TransactionID)
+	assert.Equal(t, "new error", err0.Exception.Message)
+	assert.Equal(t, "hello world", err0.Log.Message)
+	assert.Equal(t, "mockFuncError", err0.Culprit)
+	assert.Equal(t, "mockFuncError", err0.Exception.Stacktrace[0].Function)
+	assert.Equal(t, "(*Logger).log", err0.Log.Stacktrace[0].Function)
+	assert.Equal(t, "error", err0.Log.Level)
+	assert.Equal(t, "", err0.Log.LoggerName)
+	assert.Equal(t, "", err0.Log.ParamMessage)
+	assert.NotEmpty(t, err0.Log.Stacktrace)
+	assert.NotEmpty(t, err0.Exception.Stacktrace)
+	assert.NotEqual(t, err0.Log.Stacktrace, err0.Exception.Stacktrace)
 }
 
-func TestHandlerWithError(t *testing.T) {
+// it should add trace attributes and report an two errors
+func TestHandlerMultiErrorWithContext(t *testing.T) {
 	tracer, transport := transporttest.NewRecorderTracer()
 	defer tracer.Close()
-
 	var buf bytes.Buffer
-
 	h := newApmslogHandler(&buf, tracer)
 	logger := slog.New(h)
 
-	logger.Error("hello world", "error", mockFuncError())
+	// create transactions and spans
+	tx := tracer.StartTransaction("name", "type")
+	ctx := apm.ContextWithTransaction(context.Background(), tx)
+	span, ctx := apm.StartSpan(ctx, "name", "type")
 
+	logger.ErrorContext(ctx, "hello world", "error", mockFuncError(), "err", errors.New("new error"))
+
+	span.End()
+	tx.End()
+
+	var msg MsgWithTrace
+	err := json.Unmarshal([]byte(buf.String()), &msg)
+    assert.NoError(t, err)
+
+	// assert that we added the correct traces to buff
+	assert.Equal(t, tx.TraceContext().Trace.String(), msg.TraceId)
+	assert.Equal(t, span.TraceContext().Span.String(), msg.SpanId)
+	assert.Equal(t, tx.TraceContext().Span.String(), msg.TransactionId)
+
+	// assert msg is as expected
+	assert.Equal(t,
+		fmt.Sprintf(`{"time":"1970-01-01T00:00:00Z","level":"ERROR","msg":"hello world","error":"new error","err":"new error","trace.id":"%s","transaction.id":"%s","span.id":"%s"}`,
+			msg.TraceId,
+			msg.TransactionId,
+			msg.SpanId,
+		)+"\n",
+		buf.String(),
+	)
+
+	// assert we reported one error with traces to apm
 	tracer.Flush(nil)
 	payloads := transport.Payloads()
+	assert.Len(t, payloads.Transactions, 1)
+	assert.Len(t, payloads.Spans, 1)
+	assert.Len(t, payloads.Errors, 2)
+
+	// assert first apm error fields are correct
+	err0 := payloads.Errors[0]
+	assert.Equal(t, payloads.Spans[0].ID, err0.ParentID)
+	assert.Equal(t, payloads.Transactions[0].TraceID, err0.TraceID)
+	assert.Equal(t, payloads.Transactions[0].ID, err0.TransactionID)
+	assert.Equal(t, "new error", err0.Exception.Message)
+	assert.Equal(t, "hello world", err0.Log.Message)
+	assert.Equal(t, "mockFuncError", err0.Culprit)
+	assert.Equal(t, "mockFuncError", err0.Exception.Stacktrace[0].Function)
+	assert.Equal(t, "(*Logger).log", err0.Log.Stacktrace[0].Function)
+	assert.Equal(t, "error", err0.Log.Level)
+	assert.Equal(t, "", err0.Log.LoggerName)
+	assert.Equal(t, "", err0.Log.ParamMessage)
+	assert.NotEmpty(t, err0.Log.Stacktrace)
+	assert.NotEmpty(t, err0.Exception.Stacktrace)
+	assert.NotEqual(t, err0.Log.Stacktrace, err0.Exception.Stacktrace)
+
+	// assert second apm error fields are correct
+	err1 := payloads.Errors[1]
+	assert.Equal(t, payloads.Spans[0].ID, err1.ParentID)
+	assert.Equal(t, payloads.Transactions[0].TraceID, err1.TraceID)
+	assert.Equal(t, payloads.Transactions[0].ID, err1.TransactionID)
+	assert.Equal(t, "new error", err1.Exception.Message)
+	assert.Equal(t, "hello world", err1.Log.Message)
+	assert.Equal(t, "TestHandlerMultiErrorWithContext", err1.Culprit)
+	assert.Equal(t, "TestHandlerMultiErrorWithContext", err1.Exception.Stacktrace[0].Function)
+	assert.Equal(t, "(*Logger).log", err1.Log.Stacktrace[0].Function)
+	assert.Equal(t, "error", err1.Log.Level)
+	assert.Equal(t, "", err1.Log.LoggerName)
+	assert.Equal(t, "", err1.Log.ParamMessage)
+	assert.NotEmpty(t, err1.Log.Stacktrace)
+	assert.NotEmpty(t, err1.Exception.Stacktrace)
+	assert.NotEqual(t, err1.Log.Stacktrace, err1.Exception.Stacktrace)
+}
+
+func TestHandlerCustomErrorAttrNoContext(t *testing.T) {
+	// create apmslog handler with custom error record attributes to record
+	tracer, transport := transporttest.NewRecorderTracer()
+	defer tracer.Close()
+	var buf bytes.Buffer
+	apmHandler := apmslog.NewApmHandler(
+		apmslog.WithTracer(tracer),
+		apmslog.WithErrorRecordAttrs([]string{"http_error"}),
+		apmslog.WithHandler(slog.NewJSONHandler(&buf, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if a.Key == slog.TimeKey {
+					a.Value = slog.TimeValue(time.Unix(0, 0).UTC())
+				}
+				return a
+			},
+		})),
+	)
+	logger := slog.New(apmHandler)
+
+	logger.Error("hello world", "http_error", mockFuncError(), "do_not_report_error", errors.New("do not report me"))
+
+	// assert msg looks as expected
+	assert.Equal(t, `{"time":"1970-01-01T00:00:00Z","level":"ERROR","msg":"hello world","http_error":"new error","do_not_report_error":"do not report me"}`+"\n", buf.String())
+
+	// assert we reported one error with traces to apm
+	tracer.Flush(nil)
+	payloads := transport.Payloads()
+	assert.Empty(t, payloads.Transactions, 0)
+	assert.Empty(t, payloads.Spans, 0)
 	assert.Len(t, payloads.Errors, 1)
 
+	// assert apm error fields are correct
 	err0 := payloads.Errors[0]
 	assert.Equal(t, "new error", err0.Exception.Message)
 	assert.Equal(t, "hello world", err0.Log.Message)
 	assert.Equal(t, "mockFuncError", err0.Culprit)
+	assert.Equal(t, "mockFuncError", err0.Exception.Stacktrace[0].Function)
+	assert.Equal(t, "(*Logger).log", err0.Log.Stacktrace[0].Function)
+	assert.Equal(t, "error", err0.Log.Level)
+	assert.Equal(t, "", err0.Log.LoggerName)
+	assert.Equal(t, "", err0.Log.ParamMessage)
 	assert.NotEmpty(t, err0.Log.Stacktrace)
 	assert.NotEmpty(t, err0.Exception.Stacktrace)
 	assert.NotEqual(t, err0.Log.Stacktrace, err0.Exception.Stacktrace)
-	assert.Equal(t, "mockFuncError", err0.Exception.Stacktrace[0].Function)
-	assert.Equal(t, "(*Logger).log", err0.Log.Stacktrace[0].Function)
 }
 
-func mockFuncError() error {
-	return errors.New("new error")
-}
-
+// assert that logger still works if tracer is closed
 func TestHandlerTracerClosed(t *testing.T) {
 	tracer, _ := transporttest.NewRecorderTracer()
 	tracer.Close()
